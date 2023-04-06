b93f451a746fa3b8965dfde887ff2e005aec1cb6
anhvu711 <92658215+anhvu711@users.noreply.github.com>
2023-04-06 15:15:38 +0700

    Add files via upload

---
+++ session.php
@@ -0,0 +1,8 @@
+<?php   
+    session_start();
+    echo $_SESSION["username"];
+    echo $_SESSION["password"];
+    echo $_SESSION["status"];
+    echo $_SESSION["name"];
+    echo $_SESSION["chucvu"];
+?> 
