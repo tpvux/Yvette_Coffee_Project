f9900a417f56a385d44609fce3ea4f7ff3c4337f
anhvu711 <92658215+anhvu711@users.noreply.github.com>
2023-04-19 16:26:40 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once "../db_connect.php";
+
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+    /*$sql1 = "SELECT * FROM `nhan_vien`";
+    $result1 = mysqli_query($conn, $sql1); 
+    $row1 = mysqli_fetch_array($result1);
+    $manv = $row1['MaNV'];*/
+    echo $id;
+    $sql = "DELETE FROM `nhan_vien` WHERE `MaNV` = $id";
+    $result = mysqli_query($conn, $sql );
+    echo $sql;
+    header("location: employ_info.php");
+
+}
+?>
