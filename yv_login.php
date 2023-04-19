de0f90665b8fecd11ecd66f5ea5f803fcd751631
anhvu711 <92658215+anhvu711@users.noreply.github.com>
2023-04-19 16:32:55 +0700

    Add files via upload

--- yv_login.php
+++ yv_login.php
@@ -19,7 +19,7 @@
         $usernameTxt = $_POST["uname"];
         $passwordTxt = md5(md5($_POST["psw"])).""; 
 
-        require_once "./db_connect.php";
+        require_once "db_connect.php";
         $sql = "SELECT * FROM tai_khoan WHERE TenDangNhap = ? AND MatKhau = ?";       
         $stmt = mysqli_prepare($conn, $sql);
         mysqli_stmt_bind_param($stmt, 'ss', $usernameTxt, $passwordTxt );
@@ -43,12 +43,12 @@
                 </script>
                 <?php
             }
-            else
+        else
             {
                 ?>
                 <script>
                     alert("Sai tài khoản hoặc mật khẩu. Vui lòng nhập lại");
-                    location.assign("./index.php");
+                    location.assign("./yvette_website.php");
                 </script>
                 <?php
             }
